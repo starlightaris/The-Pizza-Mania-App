de39c229ca0da3c7ff0f0d731590ffc35a508728
Pavani Senulya <senulya.pavani@gmail.com>
2025-09-07 00:53:38 +0530

    bottom nav switching tab fix

--- app/src/main/java/com/nibm/pizzamaniamobileapp/MainActivity.java
+++ app/src/main/java/com/nibm/pizzamaniamobileapp/MainActivity.java
@@ -1,13 +1,18 @@
 package com.nibm.pizzamaniamobileapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -25,5 +30,45 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigatinView = findViewById(R.id.bottomNavView);
         frameLayout = findViewById(R.id.frameLayout);
+
+        bottomNavigatinView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                int itemId = menuItem.getItemId();
+
+                if(itemId == R.id.navHome){
+                    loadFragment(new HomeFragment(), false);
+
+                } else if (itemId == R.id.navCart) {
+                    loadFragment(new CartFragment(), false);
+
+                } else if (itemId == R.id.navActivity) {
+                    loadFragment(new ActivityFragment(), false);
+
+                } else { //nav profile
+                    loadFragment(new ProfileFragment(), false);
+
+                }
+
+                return true;
+            }
+        });
+
+        loadFragment(new HomeFragment(), true);
+    }
+
+    private void loadFragment(Fragment fragment, boolean isAppInitialized){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (isAppInitialized) {
+            fragmentTransaction.add(R.id.frameLayout, fragment);
+        } else {
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
+        fragmentTransaction.commit();
+
     }
 }
