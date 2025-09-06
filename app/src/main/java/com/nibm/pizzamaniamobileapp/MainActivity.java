0488802da91a73ffed76c0a9a1eb8a897a9a5b99
Pavani Senulya <senulya.pavani@gmail.com>
2025-09-07 00:20:11 +0530

    bottom nav icons added

--- app/src/main/java/com/nibm/pizzamaniamobileapp/MainActivity.java
+++ app/src/main/java/com/nibm/pizzamaniamobileapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.nibm.pizzamaniamobileapp;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigatinView;
+    private FrameLayout frameLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        bottomNavigatinView = findViewById(R.id.bottomNavView);
+        frameLayout = findViewById(R.id.frameLayout);
     }
 }
