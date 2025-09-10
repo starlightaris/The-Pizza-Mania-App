16bb880a9a8e34331897af101eacff56225476e8
Pavani Senulya <senulya.pavani@gmail.com>
2025-09-10 23:12:12 +0530

    category/horizontal menu bar

--- app/src/main/java/com/nibm/pizzamaniamobileapp/view/HomeFragment.java
+++ app/src/main/java/com/nibm/pizzamaniamobileapp/view/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -13,6 +14,7 @@ import android.view.ViewGroup;
 import com.nibm.pizzamaniamobileapp.R;
 import com.nibm.pizzamaniamobileapp.model.HomeHoriModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import adapters.HomeHoriAdapter;
@@ -28,7 +30,22 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        home
+        homeHorizontalRec = root.findViewById(R.id.home_hori_rec);
+
+        homeHoriModelList = new ArrayList<>();
+
+        homeHoriModelList.add(new HomeHoriModel(R.drawable.icon_pizza, "Pizza"));
+        homeHoriModelList.add(new HomeHoriModel(R.drawable.icon_burgers, "Burgers"));
+        homeHoriModelList.add(new HomeHoriModel(R.drawable.icon_fries, "Sides"));
+        homeHoriModelList.add(new HomeHoriModel(R.drawable.icon_beverages, "Drinks"));
+        homeHoriModelList.add(new HomeHoriModel(R.drawable.icon_icecream, "Dessert"));
+        homeHoriModelList.add(new HomeHoriModel(R.drawable.icon_cupcake, "Cake"));
+
+        homeHoriAdapter = new HomeHoriAdapter(getActivity(), homeHoriModelList);
+        homeHorizontalRec.setAdapter(homeHoriAdapter);
+        homeHorizontalRec.setLayoutManager((new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL, false)));
+        homeHorizontalRec.setHasFixedSize(true);
+        homeHorizontalRec.setNestedScrollingEnabled(false);
         return root;
     }
 }
