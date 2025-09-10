d4be4fad87115b3f367c1cc8d21b55c4e54de2e8
Pavani Senulya <senulya.pavani@gmail.com>
2025-09-10 22:34:50 +0530

    header

---
+++ app/src/main/java/adapters/HomeHoriAdapter.java
@@ -0,0 +1,58 @@
+package adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.nibm.pizzamaniamobileapp.R;
+import com.nibm.pizzamaniamobileapp.model.HomeHoriModel;
+
+import java.util.List;
+
+public class HomeHoriAdapter extends RecyclerView.Adapter<HomeHoriAdapter.ViewHolder> {
+
+    Context context;
+    List<HomeHoriModel> list;
+
+    public HomeHoriAdapter(Context context, List<HomeHoriModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.home_horizontal_category, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.imageView.setImageResource(list.get(position).getImage());
+        holder.name.setText(list.get(position).getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView name;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.hori_pizza_img);
+            name = itemView.findViewById(R.id.hori_pizza_txt);
+        }
+    }
+}
--- app/src/main/java/com/nibm/pizzamaniamobileapp/view/HomeFragment.java
+++ app/src/main/java/com/nibm/pizzamaniamobileapp/view/HomeFragment.java
@@ -2,20 +2,33 @@ package com.nibm.pizzamaniamobileapp.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.nibm.pizzamaniamobileapp.R;
+import com.nibm.pizzamaniamobileapp.model.HomeHoriModel;
+
+import java.util.List;
+
+import adapters.HomeHoriAdapter;
 
 public class HomeFragment extends Fragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    RecyclerView homeHorizontalRec;
+    List<HomeHoriModel> homeHoriModelList;
+    HomeHoriAdapter homeHoriAdapter;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        home
+        return root;
     }
 }
