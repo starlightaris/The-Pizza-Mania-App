21604d35493d85f9b57d77a8d5fec5afa151888c
Pavani Senulya <senulya.pavani@gmail.com>
2025-09-15 20:22:01 +0530

    removed kotlin files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.google.gms.google.services)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
